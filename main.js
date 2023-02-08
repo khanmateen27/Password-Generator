4d9360bbdfd466a87aeb133c99c6c246d425d68e
khanmateen27 <khanmateen2712@gmail.com>
2023-02-08 20:03:13 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+//sets for pwd
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerSet = "abcdefghijklmnopqrstuvwxyz"
+const numberSet = "1234567890"
+const symbolSet = "~!@#$%^&*()_+-{}|?/.><";
+
+//selectors
+const passBox = document.getElementById("pass-box")
+const totalChar = document.getElementById("total-char")
+const upperInput = document.getElementById("upper-case")
+const lowerInput = document.getElementById("lower-case")
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
+
+//random generator
+const getRandomData = (dataSet) => {
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+}
+
+//checked conditions
+const generatePassword = (password = "") => {
+    if(upperInput.checked){
+        password += getRandomData(upperSet)
+    }
+    if(lowerInput.checked){
+        password += getRandomData(lowerSet)
+    }
+    if(numberInput.checked){
+        password += getRandomData(numberSet)
+    }
+    if(symbolInput.checked){
+        password += getRandomData(symbolSet)
+    }
+
+    //for generting password for the given size
+    if(password.length < totalChar.value){
+        //recursively calling of the function to get desired length of the password
+        return generatePassword(password);
+    }
+    passBox.innerText = truncateString(password , totalChar.value)
+    
+}
+
+document.getElementById("btn").addEventListener("click" , function(){
+    generatePassword();
+})
+
+function truncateString(str , num){
+    if(str.length > num){
+        let substr = str.substring(0,num);
+        return substr;
+    }else {
+        return str;
+    }
+}
